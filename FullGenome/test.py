84e7c512531400f2f9de83168b5555165cadd1d0
Mihail Vasilev <mickvav@gmail.com>
2019-01-11 00:19:09 +0000

    Added context-searching function.

--- FullGenome/test.py
+++ FullGenome/test.py
@@ -1,14 +1,22 @@
-#!/usr/bin/python
-
+#!/usr/bin/python3
+#
+#  Example of library usage.
+# 
+#  expecting to have fasta file as argument, file should contain multiple
+#  chromosomes as separate entries.
+#
+#
 from sys import argv
 from getcontext import FullGenome
 
 G=FullGenome(argv[1])
 
-print G.getcontext(0,10)
-print G.getcontext(0,100)
-print G.getcontext(0,2)
-print G.getcontext(0,3)
-print G.getcontext(0,4)
-print G.getcontext(0,5)
-print G.getcontext(0,6)
+print(G.getcontext(0,10))
+print(G.getcontext(0,100))
+print(G.getcontext(0,2))
+print(G.getcontext(0,3))
+print(G.getcontext(0,4))
+print(G.getcontext(0,5))
+print(G.getcontext(0,6))
+print("AAA nearest to 10 in 1'st chromosome:")
+print(G.findnearestcontext("AAA",0,10))
