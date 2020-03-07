aa005016b38f29c6195fbc0a37ecf129c3a863d4
Mikhail Vasilyev and Ershova Anna <mickvav@you-know-google-mail.com>
2020-03-07 18:27:05 +0000

    Added connect_nfs.sh

---
+++ marine/ec2/connect_nfs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo mkdir /mnt/data
+mkdir /home/ubuntu/reference_contig_fasta
+cat >> /etc/fstab << EOF
+ip-172-31-24-63:/mnt/data /mnt/data nfs  ro 0 0
+ip-172-31-24-63:/home/ubuntu/reference_contig_fasta /home/ubuntu/reference_contig_fasta nfs  rw,sync 0 0
+EOF
+sudo apt install nfs-common
+sudo mount -a
