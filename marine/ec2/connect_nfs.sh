1785cd3061d6b132ef99d7e4768fd8fa3bd4af66
Ubuntu <ubuntu@ip-172-31-49-209.us-west-2.compute.internal>
2020-03-08 13:15:11 +0000

    Auto Yes

--- marine/ec2/connect_nfs.sh
+++ marine/ec2/connect_nfs.sh
@@ -5,5 +5,5 @@ cat >> /etc/fstab << EOF
 ip-172-31-24-63:/mnt/data /mnt/data nfs  ro 0 0
 ip-172-31-24-63:/home/ubuntu/reference_contig_fasta /home/ubuntu/reference_contig_fasta nfs  rw,sync 0 0
 EOF
-sudo apt install nfs-common
+sudo apt -y install nfs-common
 sudo mount -a
--- marine/ec2/install_nano_instance.sh
+++ marine/ec2/install_nano_instance.sh
@@ -2,13 +2,13 @@
 
 wget https://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.4.1/bowtie2-2.4.1-linux-x86_64.zip/download
 sudo apt-get update 
-sudo apt-get install build-essential unzip
+sudo apt-get -y install build-essential unzip
 mv download bowtie2.zip
 unzip bowtie2.zip 
 
 wget https://github.com/samtools/samtools/releases/download/1.10/samtools-1.10.tar.bz2
 tar -xvjf samtools-1.10.tar.bz2 
-sudo apt-get install ncurses-dev zlib1g-dev libbz2-dev liblzma-dev libcurl4-openssl-dev libssl-dev
+sudo apt-get -y install ncurses-dev zlib1g-dev libbz2-dev liblzma-dev libcurl4-openssl-dev libssl-dev
 cd samtools-1.10/
 ./configure --prefix=/usr/local
 make && sudo make install
