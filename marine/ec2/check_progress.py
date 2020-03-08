52c5217ce0085e562278520196daaf7d466dbea7
Mikhail Vasilyev <mickvav@guess-it-yourself.com>
2020-03-08 18:16:52 +0000

    Performance measurement tool.

---
+++ marine/ec2/check_progress.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import subprocess
+import re
+from os import stat, readlink
+from time import sleep
+from sys import argv
+if argv[1]:
+    interval=float(argv[1])
+else:
+    interval=5
+def bowtie_pid_progress(pid):
+    stat1 = stat(f"/proc/{pid}/fd/3")
+    stat2 = stat(f"/proc/{pid}/fd/4")
+    with open(f"/proc/{pid}/fdinfo/3") as f:
+        line = f.readline()
+        pos1=int(line[5:].strip())
+    with open(f"/proc/{pid}/fdinfo/4") as f:
+        line = f.readline()
+        pos2=int(line[5:].strip())
+    return (stat1[6], stat2[6], pos1, pos2)
+
+lines = subprocess.check_output('ps -A | grep bowtie2-align-s', shell=True, universal_newlines=True)
+state = {}
+for line in lines.split("\n"):
+    try:
+        (pid, pts, time, name) = re.split("\s+",line)
+        (s1,s2,p1,p2) = bowtie_pid_progress(pid)
+        print(pid, pts, 50*(p1/s1+p2/s2), readlink(f"/proc/{pid}/fd/3"))
+        state[pid] = p1 + p2
+    except Exception as ee:
+        pass
+
+sleep(interval)
+perf = 0
+for line in lines.split("\n"):
+    try:
+        (pid, pts, time, name) = re.split("\s+",line)
+        (s1,s2,p1,p2) = bowtie_pid_progress(pid)
+        pr= p1+p2
+        print(pid, (pr-state[pid])/interval)
+        perf += (pr-state[pid])/interval
+    except Exception as ee:
+        pass
+print(f"Total performance: {perf} bytes/s")
