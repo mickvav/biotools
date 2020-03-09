c933d4a20ab1f289015d224ccc5a29f38ab1deac
Mikhail Vasilyev and Ershova Anna <mickvav@everyoneknowswheregooglemailis.com>
2020-03-09 02:42:31 +0000

    stop.get

--- marine/ec2/get.py
+++ marine/ec2/get.py
@@ -7,6 +7,8 @@ import re
 
 with open(argv[1],"r") as f:
     for line in f.readlines():
+        if exists("/home/ubuntu/stop.get"):
+            raise RuntimeError("remove /home/ubuntu/stop.get")
         if exists(basename(line.strip())):
             print(f"File {line.strip()} exists. Skip download")
         else:
